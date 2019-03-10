c6e77ac35db10490a0489da0ac342747b3c7edce
Srajika gupta <32300828+srajika256@users.noreply.github.com>
2019-03-11 00:20:24 +0530

    changed xpath which were giving error

--- python_code.py
+++ python_code.py
@@ -6,30 +6,36 @@ from openpyxl import Workbook
 browser = webdriver.Chrome("C:\\Users\\Srajika Gupta\\Downloads\\chromedriver_win32\\chromedriver.exe")
 
 def search():
-    username = input("Enter The name of user to be searched")
+     username = input("Enter The name of user to be searched : ")
+     url = 'https://github.com/' + username
+     browser.get(url)
+    
+     repositories = browser.find_elements_by_xpath('//*[@id="js-pjax-container"]/div/div[3]/div[2]/nav/a[2]/span')
+     print("Number of Repositories : ",end=" ")
+     print(repositories[0].text)
+    
+    
+     projects = browser.find_elements_by_xpath('//*[@id="js-pjax-container"]/div/div[3]/div[2]/nav/a[3]/span')
+     print("Number of Projects : ",end=" ")
+     print(projects[0].text)   
+    
+ 
+     stars = browser.find_elements_by_xpath('//*[@id="js-pjax-container"]/div/div[3]/div[2]/nav/a[4]/span')
+     print("Stars : ",end=' ')
+     print(stars[0].text)
 
-    url = 'https://github.com/' + str(username)
-    browser.get(url)
-    
-    repositories = browser.find_elements_by_xpath('//*[@id="js-pjax-container"]/div/div[3]/div[2]/nav/a[2]/span')
-    print("Number of Repositories : ",end=" ")
-    print(repositories[0].text)
     
     
-    projects = browser.find_element_by_xpath('//*[@id="js-pjax-container"]/div/div[2]/div[2]/nav/a[3]/span')
-    print("Number of Projects : ",end=" ")
-    print(projects[0].text)
+     followers = browser.find_elements_by_xpath('//*[@id="js-pjax-container"]/div/div[3]/div[2]/nav/a[5]/span')
+     print("Followers : ",end=" ")
+     print(followers[0].text)
     
-    stars = browser.find_element_by_xpath('//*[@id="js-pjax-container"]/div/div[2]/div[2]/nav/a[4]/span')
-    print("Stars : ",end=' ')
-    print(stars[0].text)
-    
-    followers = browser.find_element_by_xpath('//*[@id="js-pjax-container"]')
-    print("Followers : ",end=" ")
-    print(followers[0].text)
-    
-    following = browser.find_element_by_xpath('//*[@id="js-pjax-container"]/div/div[2]/div[2]/nav/a[6]/span')
-    print("Following : ",end= " ")
-    print(following[0].text)
+     following = browser.find_elements_by_xpath('//*[@id="js-pjax-container"]/div/div[3]/div[2]/nav/a[6]/span')
+     print("Following : ",end= " ")
+     print(following[0].text)                                                                        
+
 
 search()
+
+
+  
